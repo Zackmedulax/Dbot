ff6240c266ec9d0ed1260ac30cc832918584c75d
dinar <dinarskt@gmail.com>
2026-07-11 21:18:59 +0700

    update follow

--- app/Dbot.js
+++ app/Dbot.js
@@ -80,7 +80,14 @@ Tekan help untuk melihat panduan lengkap.
   getFollow() {
     this.onText(commands.follow, (data, after) => {
       console.log("Fitur follow di pake " + data.from.first_name)
-      this.sendMessage(data.from.id, `${after[1]}`)
+      console.log(data)
+      this.sendMessage(data.from.id, `
+Message Id '${data.from.message_id}'
+Dari '${data.from.first_name}'
+BotOrNo '${data.from.is_bot}'
+Id Tele '${data.chat.id}'
+${after[1]}
+`)
     })
   }
   getQuote() {
